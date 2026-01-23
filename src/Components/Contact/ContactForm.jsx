a6263846cf38ebcb80c4c96f593411368b748d5c
Atif Afsar <atifafsar70@gmail.com>
2026-01-23 12:47:53 +0530

    Form is working

--- src/Components/Contact/ContactForm.jsx
+++ src/Components/Contact/ContactForm.jsx
@@ -27,7 +27,7 @@ const handleSubmit = async (e) => {
   }
 
   try {
-    const response = await fetch("http://localhost:5000/api/contact", {
+    const response = await fetch("https://learn-logix-backend-l5jz.vercel.app/api/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
